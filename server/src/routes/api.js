cdbfef96a95ca4f9f15c827d3c7b780a9fb5da4f
SuperCheese21 <ethan.shields21@gmail.com>
2020-03-13 21:24:09 -0700

    Added database connection to server, added API route for airports

--- server/src/routes/api.js
+++ server/src/routes/api.js
@@ -1,9 +1,23 @@
+import 'regenerator-runtime/runtime';
+
 import express from 'express';
+
+import Airport from '../models/airport';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('respond with a resource');
+router.get('/', async (req, res) => {
+  res.json({ message: 'API home page' });
+});
+
+router.get('/airport/:id', async (req, res) => {
+  const { id } = req.params;
+  const airport = await Airport.findOne({ _id: id }).exec();
+  if (airport) {
+    res.json(airport);
+  } else {
+    res.sendStatus(404);
+  }
 });
 
 export default router;
