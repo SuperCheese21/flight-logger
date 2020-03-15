c09cb13c6552fcbcf0dd8f6787abc5b9059235c4
SuperCheese21 <ethan.shields21@gmail.com>
2020-03-14 20:30:52 -0700

    Added airlines data file and script to add airlines to database

---
+++ server/src/models/airline.js
@@ -0,0 +1,23 @@
+import { model, Schema } from 'mongoose';
+
+import { CountrySchema } from './country';
+
+export const AirlineSchema = new Schema({
+  iata: String,
+  icao: String,
+  name: String,
+  callsign: String,
+  country: CountrySchema,
+  comments: String,
+});
+
+AirlineSchema.static('getUpdate', row => ({
+  iata: row[0],
+  icao: row[1],
+  name: row[2],
+  callsign: row[3],
+  country: { name: row[4] },
+  comments: row[5],
+}));
+
+export default model('Airline', AirlineSchema, 'airlines');
---
+++ server/src/utils/updateAirlines.js
@@ -0,0 +1,55 @@
+import 'regenerator-runtime/runtime';
+
+import { readFileSync } from 'fs';
+import url from 'url';
+
+import parse from 'csv-parse/lib/sync';
+import mongoose from 'mongoose';
+
+import Airline from '../models/airline';
+
+import { mongodb as dbConfig } from '../../config.json';
+
+const updateData = async () => {
+  console.log(`Removing airlines from database...`);
+  const res = await Airline.deleteMany({}).exec();
+  console.log(`  Done! Number of documents removed: ${res.deletedCount}`);
+
+  console.log(`Fetching data from airlines.csv...`);
+  const csv = readFileSync('./data/airlines.csv');
+  console.log(`  Done!`);
+
+  const data = parse(csv, { skip_empty_lines: true }).slice(1);
+
+  await Promise.all(
+    data.map(row => {
+      const update = Airline.getUpdate(row);
+      const airline = new Airline(update);
+      return airline.save(e =>
+        e ? console.error(e) : console.log(`  Upserted ${row[0]}/${row[1]}`),
+      );
+    }),
+  );
+};
+
+// Get formatted mongodb URL
+const mongoURL = url.format(dbConfig);
+
+// Configure database connection
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useUnifiedTopology', true);
+
+// Initiate connection to database
+console.log(`Connecting to ${mongoURL}...`);
+mongoose.connect(mongoURL);
+
+// Set connection event listeners
+const db = mongoose.connection;
+db.on('error', () => {
+  console.error('  connection error');
+});
+db.once('open', async () => {
+  console.log('  Connected!');
+  updateData();
+});
