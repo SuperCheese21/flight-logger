5eb04aac89a010f6a7bf5d1b23e622b0fad009e3
SuperCheese21 <ethan.shields21@gmail.com>
2020-04-12 02:29:49 -0700

    Added airline logo url fetch, refactoring

--- server/src/db/parseData.js
+++ server/src/db/parseData.js
@@ -1,13 +1,19 @@
+import axios from 'axios';
 import cheerio from 'cheerio';
 import parse from 'csv-parse/lib/sync';
 
-export const parseOurAirportsData = data =>
-  parse(data, { skip_empty_lines: true }).slice(1);
+export const getLogoURL = async href => {
+  const url = `https://en.wikipedia.org${href}`;
+  try {
+    const res = await axios.get(url);
+    const $ = cheerio.load(res.data);
+    const src = $('.infobox img').attr('src');
 
-export const parseWikipediaData = data => {
-  const html = data.replace(/\r?\n|\r|N\/A|n\/a|\*/g, '');
-  const $ = cheerio.load(html);
-  return $('.wikitable tr').toArray();
+    return src ? `https:${src}` : '';
+  } catch (err) {
+    console.error(`  ${url} - Logo not found (${err.message})`);
+    return '';
+  }
 };
 
 export const getText = node => {
@@ -16,4 +22,13 @@ export const getText = node => {
     .remove()
     .end()
     .text();
+};
+
+export const parseOurAirportsData = data =>
+  parse(data, { skip_empty_lines: true }).slice(1);
+
+export const parseWikipediaData = data => {
+  const html = data.replace(/\r?\n|\r|N\/A|n\/a|\*/g, '');
+  const $ = cheerio.load(html);
+  return $('.wikitable tr').toArray();
 };
