f600d1fd8fb38ee084bed012c4ce8b6534fbc96e
SuperCheese21 <ethan.shields21@gmail.com>
2020-04-23 01:07:43 -0700

    Added pagination to country and region endpoints

--- server/src/routes/api.js
+++ server/src/routes/api.js
@@ -35,38 +35,12 @@ router.get('/airports', paginatedResults(Airport), (req, res) => {
   res.json(res.paginatedResults);
 });
 
-router.get('/countries', async (req, res) => {
-  const query = Country.find({});
-  const countries = await query.exec();
-  res.json(countries);
+router.get('/countries', paginatedResults(Country), (req, res) => {
+  res.json(res.paginatedResults);
 });
 
-router.get('/countries/:id', async (req, res) => {
-  const { id } = req.params;
-  const query = Country.findOne({ _id: id });
-  const country = await query.exec();
-  if (country) {
-    res.json(country);
-  } else {
-    res.sendStatus(404);
-  }
-});
-
-router.get('/regions', async (req, res) => {
-  const query = Region.find({});
-  const regions = await query.exec();
-  res.json(regions);
-});
-
-router.get('/regions/:id', async (req, res) => {
-  const { id } = req.params;
-  const query = Region.findOne({ _id: id });
-  const region = await query.exec();
-  if (region) {
-    res.json(region);
-  } else {
-    res.sendStatus(404);
-  }
+router.get('/regions', paginatedResults(Region), (req, res) => {
+  res.json(res.paginatedResults);
 });
 
 export default router;
