f2d9a2c0af6e8808dcbd2c6abb16dad78cb8f6c5
SuperCheese21 <ethan.shields21@gmail.com>
2020-06-07 02:55:06 -0700

    Moved flights endpoints to separate file

---
+++ server/src/routes/api/flights.js
@@ -0,0 +1,71 @@
+import express from 'express';
+import passport from 'passport';
+
+import {
+  saveFlight,
+  deleteFlight,
+  getFlightById,
+  updateFlight,
+} from '../../models/flight';
+
+const router = express.Router();
+
+router.use(passport.authenticate('jwt', { session: false }));
+
+router.post('/', async (req, res) => {
+  const userId = req.user._id;
+  try {
+    const flight = await saveFlight(userId, req.body);
+    res.json(flight);
+  } catch ({ message, name }) {
+    if (name === 'ValidationError') {
+      res.status(400);
+    } else {
+      res.status(500);
+    }
+    res.json({ message });
+  }
+});
+
+router.get('/:id', async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const flight = await getFlightById(id);
+    if (!flight) {
+      next();
+    }
+    res.json(flight);
+  } catch ({ message }) {
+    res.status(500).json({ message });
+  }
+});
+
+router.patch('/:id', async (req, res, next) => {
+  const { id } = req.params;
+  const userId = req.user._id;
+  try {
+    const flight = await updateFlight(id, userId, req.body);
+    if (!flight) {
+      next();
+    }
+    res.json(flight);
+  } catch ({ message }) {
+    res.status(500).json({ message });
+  }
+});
+
+router.delete('/:id', async (req, res, next) => {
+  const { id } = req.params;
+  const userId = req.user._id;
+  try {
+    const flight = await deleteFlight(id, userId);
+    if (!flight) {
+      next();
+    }
+    res.json(flight);
+  } catch ({ message }) {
+    res.status(500).json({ message });
+  }
+});
+
+export default router;
--- server/src/routes/api/index.js
+++ server/src/routes/api/index.js
@@ -1,16 +1,10 @@
 import express from 'express';
-import passport from 'passport';
 import swaggerUi from 'swagger-ui-express';
 
 import authRouter from './auth';
 import dataRouter from './data';
+import flightsRouter from './flights';
 
-import {
-  saveFlight,
-  deleteFlight,
-  getFlightById,
-  updateFlight,
-} from '../../models/flight';
 import apiSpec from '../../../openapi.json';
 
 const router = express.Router();
@@ -21,64 +15,7 @@ router.get('/', (req, res) => {
 router.use('/docs', swaggerUi.serve, swaggerUi.setup(apiSpec));
 router.use('/auth', authRouter);
 router.use('/data', dataRouter);
-
-router.use(passport.authenticate('jwt', { session: false }));
-
-router.post('/flights', async (req, res) => {
-  const userId = req.user._id;
-  try {
-    const flight = await saveFlight(userId, req.body);
-    res.json(flight);
-  } catch ({ message, name }) {
-    if (name === 'ValidationError') {
-      res.status(400);
-    } else {
-      res.status(500);
-    }
-    res.json({ message });
-  }
-});
-
-router.get('/flights/:id', async (req, res, next) => {
-  const { id } = req.params;
-  try {
-    const flight = await getFlightById(id);
-    if (!flight) {
-      next();
-    }
-    res.json(flight);
-  } catch ({ message }) {
-    res.status(500).json({ message });
-  }
-});
-
-router.patch('/flights/:id', async (req, res, next) => {
-  const { id } = req.params;
-  const userId = req.user._id;
-  try {
-    const flight = await updateFlight(id, userId, req.body);
-    if (!flight) {
-      next();
-    }
-    res.json(flight);
-  } catch ({ message }) {
-    res.status(500).json({ message });
-  }
-});
-
-router.delete('/flights/:id', async (req, res, next) => {
-  const { id } = req.params;
-  const userId = req.user._id;
-  try {
-    const flight = await deleteFlight(id, userId);
-    if (!flight) {
-      next();
-    }
-    res.json(flight);
-  } catch ({ message }) {
-    res.status(500).json({ message });
-  }
-});
+router.use('/flights', flightsRouter);
 
 router.use('*', (req, res) => {
   res.status(404).json({ message: 'Not Found' });
