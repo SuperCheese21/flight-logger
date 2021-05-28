4ff4974616aa1db039d9f33ce30132a7ab5bc0f7
SuperCheese21 <ethan.shields21@gmail.com>
2021-05-28 11:18:54 -0700

    Refactored database update script

--- server/src/db/update.js
+++ server/src/db/update.js
@@ -1,71 +1,103 @@
 import 'regenerator-runtime/runtime';
 
-import axios from 'axios';
 import Promise from 'bluebird';
 import { argv } from 'yargs';
 
 import connectDatabase from './connect';
 import models from '../models';
+import { fetchData } from '../utils/serverUtils';
 
+const LOGGING_INTERVAL = 1000;
 const PROMISE_CONCURRENCY = 50;
 
-const updateCollection = async (model, newData) => {
-  console.log(`Retrieving old data from database...`);
-  const oldData = await model.find({}).exec();
+const updateCollection = async (model, newDocuments) => {
+  // Fetch old documents from database
+  console.log(`Fetching old documents from database...`);
+  const oldDocuments = await model.find({}, e => e && console.error(e));
+  console.log(`  Retrieved ${oldDocuments.length} documents`);
+
+  // Iterate through old documents and find the ones that don't appear in the new documents
+  const documentsToDelete = oldDocuments.filter(
+    ({ _id }) => !newDocuments.find(({ _id: newId }) => _id === newId),
+  );
+
+  // Delete all obsolete documents from database
+  console.log(`Removing ${documentsToDelete.length} obsolete documents...`);
+  await Promise.all(
+    documentsToDelete.map(({ _id }) =>
+      model.deleteOne({ _id }, e => e && console.error(e)),
+    ),
+  );
   console.log('  Done!');
 
-  console.log('Removing obsolete documents...');
+  // Upsert new documents into database
+  console.log(`Upserting ${newDocuments.length} documents...`);
+  let count = 0;
   await Promise.all(
-    oldData.reduce((acc, { _id }) => {
-      if (!newData.find(({ _id: newId }) => _id === newId)) {
-        const query = model.deleteOne({ _id }, e =>
-          e ? console.error(e) : console.log(`  Deleted ${_id}`),
-        );
-        acc.push(query.exec());
-      }
-      return acc;
-    }, []),
+    newDocuments.map(document =>
+      model.findByIdAndUpdate(document._id, document, { upsert: true }, e => {
+        count += 1;
+        if (e) console.error(e);
+        else if (
+          count === newDocuments.length ||
+          count % LOGGING_INTERVAL === 0
+        ) {
+          console.log(`  ${count}/${newDocuments.length}`);
+        }
+      }),
+    ),
   );
-  console.log('Done!');
-
-  console.log('Creating updated documents...');
-  await Promise.all(
-    newData.map(object => {
-      const { _id } = object;
-      const query = model.findByIdAndUpdate(_id, object, { upsert: true }, e =>
-        e ? console.error(e) : console.log(`  Upserted ${_id}`),
-      );
-      return query.exec();
-    }),
-  );
-  console.log('Done!');
 };
 
-const updateData = async () => {
-  const collectionName = argv._[0];
-
-  console.log(`Updating ${collectionName} collection`);
-
-  const model = models[collectionName];
-  if (!model) {
-    return console.error(`Collection ${collectionName} not found! Skipping...`);
-  }
-
+const createDocuments = async model => {
   const { dataUrl, getUpdate, parseData } = model;
 
   console.log(`Fetching data from ${dataUrl} ...`);
-  const res = await axios.get(dataUrl);
-  console.log('  Done!');
+  const data = await fetchData(dataUrl);
+  if (!data) {
+    console.error('Data source not available. Skipping...');
+    return [];
+  }
 
-  const rows = parseData(res.data);
+  const rows = parseData(data);
+  console.log(`  Retrieved ${rows.length} rows`);
 
   console.log('Creating new documents...');
-  const data = await Promise.map(rows, item => getUpdate(item), {
+  const allDocuments = await Promise.map(rows, item => getUpdate(item), {
     concurrency: PROMISE_CONCURRENCY,
   });
-  const documents = data.filter(item => item);
+  const documents = allDocuments.filter(document => document);
+  console.log(`  Created ${documents.length} documents`);
 
-  return updateCollection(model, documents);
+  return documents;
+};
+
+const updateData = () => {
+  // Parse command line args and filter out collection names that don't exist
+  const collectionsToUpdate = argv._[0].split(',').filter(name => models[name]);
+
+  if (collectionsToUpdate.length) {
+    console.log(collectionsToUpdate);
+  } else {
+    console.log('No collections found');
+  }
+
+  // Iterate over collection names to update each collection
+  return Promise.map(
+    collectionsToUpdate,
+    async name => {
+      console.log(`\nUpdating ${name}`);
+
+      const model = models[name];
+      const newDocuments = await createDocuments(model);
+      await updateCollection(model, newDocuments);
+
+      console.log(`Finished updating ${name}`);
+    },
+    {
+      concurrency: 1,
+    },
+  );
 };
 
 // Initialize Database
