891cd31cabb8cc13803fe43347ce3f4f22512f58
SuperCheese21 <ethan.shields21@gmail.com>
2021-09-18 11:45:53 -0700

    Modified commands

--- client/src/common/live-search/constants.js
+++ client/src/common/live-search/constants.js
@@ -4,18 +4,6 @@ export const INPUT_SIZES = {
   lg: 'lg',
 };
 
-export const DROPDOWN_MENU_WIDTHS = {
-  [INPUT_SIZES.sm]: 250,
-  [INPUT_SIZES.md]: 300,
-  [INPUT_SIZES.lg]: 400,
-};
-
-export const DROPDOWN_ITEM_HEIGHTS = {
-  [INPUT_SIZES.sm]: 25,
-  [INPUT_SIZES.md]: 30,
-  [INPUT_SIZES.lg]: 50,
-};
-
 export const DROPDOWN_THEMES = {
   [INPUT_SIZES.sm]: {
     fontSize: 0.7,
